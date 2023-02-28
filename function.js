291a56253368f0b3e5d77f4e4c7889a93d7773c7
Ferniche <fer021078@hotmail.com>
2023-02-27 22:12:48 -0300

    primer commit

---
+++ function.js
@@ -0,0 +1,18 @@
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+/** Bibliografia utilizada para este preyecto:
+ * Pdf curso Ticmas Front end
+ * https://fonts.google.com/
+ * https://fontawesome.com/
+ * https://www.w3schools.com/
+ * https://developer.mozilla.org/
+ * Tutoriales en Youtube
+ */
